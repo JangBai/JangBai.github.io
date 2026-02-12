141f85e5348b634c22d6dba2a1a94ab5e4436710
JangBai <jbkim948@gmail.com>
2026-02-12 16:53:04 +0900

    feat: tailwind 기본 설정 및 prettier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-black flex justify-center items-center h-screen">
-        <h1 className="text-4xl font-bold text-white text-center">
-          Tailwind Test
-        </h1>
-      </div>
+      <main className="bg-black">
+        <div className="mx-auto h-screen w-2/5 max-w-screen-lg bg-black px-5">
+          <h1 className="mb-2 block text-center text-white">h1 Tailwind</h1>
+          <h2 className="mb-2 block text-center text-white">h2 Tailwind</h2>
+          <h3 className="mb-2 block text-center text-white">h3 Tailwind</h3>
+          <h4 className="mb-2 block text-center text-white">h4 Tailwind</h4>
+          <h5 className="mb-2 block text-center text-white">h5 Tailwind</h5>
+          <h6 className="mb-2 block text-center text-white">h6 Tailwind</h6>
+          <p className="mb-2 block text-center text-white">p Tailwind</p>
+        </div>
+      </main>
     </>
   );
 }
