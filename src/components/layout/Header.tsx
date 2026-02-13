af5eb84717558ea02bec09cb36e1af9ecec98ee4
JangBai <jbkim948@gmail.com>
2026-02-13 15:43:06 +0900

    feat: tsconfig 및 Vite 설정 업데이트, Header 컴포넌트 개선 및 Switch 컴포넌트 삭제

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,55 +1,35 @@
-import { useEffect, useState } from "react";
-import Switch from "../ui/Switch";
+import ThemeButton from "@/components/ui/ThemeButton";
+import { nav } from "@/data/nav";
 
 export default function Header() {
-  const [theme, setTheme] = useState("dark");
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", theme === "dark");
-  }, [theme]);
-
   return (
     <header className="fixed left-0 top-0 z-50 w-full border-neutral-200 bg-[var(--color-nav-background)] backdrop-blur-md">
       <div className="mx-auto flex max-w-4xl items-center justify-between px-6 py-4">
         {/* Logo */}
         <h1>
-          <a
-            href="#introduce"
-            className="text-lg font-semibold tracking-tight text-[var(--color-primary)]"
-          >
-            JB
+          <a href="#introduce" className="tracking-tight">
+            <span className="text-2xl font-bold text-[var(--color-primary)]">
+              JB
+            </span>
           </a>
         </h1>
 
         {/* Navigation */}
-        <nav className="flex gap-8 text-sm font-medium">
-          <a
-            href="#introduce"
-            className="text-[var(--color-nav-text)] transition-colors hover:text-[var(--color-nav-text-hover)]"
-          >
-            Introduce
-          </a>
-          <a
-            href="#experience"
-            className="text-[var(--color-nav-text)] transition-colors hover:text-[var(--color-nav-text-hover)]"
-          >
-            Experience
-          </a>
-          <a
-            href="#projects"
-            className="text-[var(--color-nav-text)] transition-colors hover:text-[var(--color-nav-text-hover)]"
-          >
-            Project
-          </a>
+        <nav className="flex gap-8">
+          {nav.map((item) => (
+            <a
+              href={item.href}
+              className="transition-colors hover:text-[var(--color-nav-text-hover)]"
+            >
+              <span className="text-sm font-medium text-[var(--color-nav-text)]">
+                {item.name}
+              </span>
+            </a>
+          ))}
         </nav>
-        {/* <button
-          className="bg-primary text-black dark:text-white"
-          onClick={() => {
-            setTheme(theme === "default" ? "dark" : "default");
-          }}
-        >
-          Click Me
-        </button> */}
-        <Switch />
+
+        {/* Theme Button */}
+        <ThemeButton />
       </div>
     </header>
   );
---
+++ src/data/nav.ts
@@ -0,0 +1,19 @@
+type nav = {
+  name: string;
+  href: string;
+};
+
+export const nav: nav[] = [
+  {
+    name: "Introduce",
+    href: "#introduce",
+  },
+  {
+    name: "Experience",
+    href: "#experience",
+  },
+  {
+    name: "Project",
+    href: "#projects",
+  },
+];
