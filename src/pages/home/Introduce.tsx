8d7113a6b7fd1e58bc2640e51aa1f66b588a603b
JangBai <jbkim948@gmail.com>
2026-02-13 17:45:30 +0900

    fix: 메인 소개 영역 1차 수정

--- src/pages/home/Introduce.tsx
+++ src/pages/home/Introduce.tsx
@@ -1,14 +1,21 @@
 export default function Introduce() {
   return (
-    <div
+    <section
       id="introduce"
-      className="flex h-screen flex-col items-center justify-center"
+      className="flex h-screen -translate-y-48 flex-col items-center justify-center gap-12"
     >
-      <div className="flex flex-col items-center justify-center gap-2 text-[var(--color-primary)]">
-        {/* <h2>KIM JANG BAE</h2>
-        <p className="text-2xl">Frontend Developer</p> */}
-        <span>1</span>
+      <div className="flex flex-col items-center justify-center gap-1 text-[var(--color-primary)]">
+        <h2 className="text-6xl font-bold">KIM JANG BAE</h2>
+        <p className="text-4xl">Frontend Developer</p>
       </div>
-    </div>
+      <div className="flex flex-col items-center justify-center">
+        <span className="text-center text-2xl text-[var(--color-primary)]">
+          문제 해결 중심의 사고와 비즈니스 이해를 바탕으로,
+        </span>
+        <span className="text-center text-2xl text-[var(--color-primary)]">
+          사용자와 서비스 모두에 가치를 더하는 프론트엔드 개발자 김장배입니다.
+        </span>
+      </div>
+    </section>
   );
 }
