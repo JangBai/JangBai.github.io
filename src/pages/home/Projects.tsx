e5c541335db8082dc8af476c92c58af49f9a714c
JangBai <jbkim948@gmail.com>
2026-02-23 15:23:38 +0900

    fix: project 카드 2차 수정 및 언어 로고 추가

--- src/pages/home/Projects.tsx
+++ src/pages/home/Projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
         Projects
       </h2>
 
-      <article className="relative w-full rounded-xl border border-white/10 bg-[var(--color-accent-soft-20)] p-6">
+      <article className="dark:[var(--color-accent-soft-20)] relative w-full rounded-xl border border-white/10 bg-[var(--color-accent-soft-20)] p-6">
         {/* 오른쪽 위 버튼 */}
         <div className="mb-6 flex flex-row place-items-end justify-between">
           {/* 제목 */}
@@ -24,7 +24,7 @@ export default function Projects() {
                 href={item.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="mr-1.5 rounded-md border border-white/10 bg-white/10 px-[0.5rem] py-[0.25rem] text-xs font-normal text-[var(--color-primary)] transition-colors duration-300 hover:bg-white/20"
+                className="mr-1.5 rounded-md border border-white/10 bg-black/10 px-[0.5rem] py-[0.25rem] text-xs font-normal text-[var(--color-primary)] transition-colors duration-300 hover:bg-white/20 dark:bg-white/10"
               >
                 {item.title}
               </a>
@@ -34,25 +34,75 @@ export default function Projects() {
 
         {/* 내용 */}
         <ul className="space-y-1 leading-relaxed text-[var(--color-primary)]">
-          <li>• 내용111111111111111111111111111111111111111111111111</li>
-          <li>• 내용222222222222222222222222222222222222222222222222</li>
-          <li>• 내용333333333333333333333333333333333333333333333333</li>
-          <li>• 내용444444444444444444444444444444444444444444444444</li>
+          <li>
+            • 센터별 페이지 특성을 고려한 SEO 구조 설계 및 메타데이터 관리
+          </li>
+          <li>
+            • 검색 API 및 검색 결과 로직 구현을 통해 사용자 검색 경험 개선
+          </li>
+          <li>
+            • 페이지 특성에 따라 SSR / ISR을 혼합 적용하고, revalidate 전략을
+            활용해 성능과 최신성의 균형을 설계
+          </li>
+          <li>
+            • 정적 export 환경에서의 서비스 운영 경험을 바탕으로 nginx 및 Docker
+            기반의 라우팅·리다이렉트 구조 구성
+          </li>
+          <li>
+            • 공통 구조를 기반으로 신규 센터를 추가할 수 있는 서비스 구조를
+            설계하여 센터 확장 및 운영 효율을 개선
+          </li>
+          <li>
+            • 운영 과정에서 변경되는 요구사항을 반영해 UI 개선과 기능 보완을
+            병행하며 서비스 완성도를 지속적으로 향상
+          </li>
         </ul>
 
         {/* 스택 */}
         <div className="mt-6 flex flex-wrap gap-2 text-xs">
-          <span className="rounded-md border border-white/10 bg-white/10 px-3 py-1">
+          <span className="flex flex-row items-center gap-2 rounded-md border border-white/10 bg-black/10 px-3 py-1 text-[var(--color-primary)] dark:bg-white/25">
+            <img
+              src="/language_logo/Next.js.svg"
+              alt="Next.js"
+              className="h-4 w-4"
+            />
             Next.js
           </span>
-          <span className="rounded-md border border-white/10 bg-white/10 px-3 py-1">
-            SEO
+          <span className="flex flex-row items-center gap-2 rounded-md border border-white/10 bg-black/10 px-3 py-1 text-[var(--color-primary)] dark:bg-white/25">
+            <img
+              src="/language_logo/React.svg"
+              alt="React"
+              className="h-4 w-4"
+            />
+            React
           </span>
-          <span className="rounded-md border border-white/10 bg-white/10 px-3 py-1">
-            Docker
+          <span className="flex flex-row items-center gap-2 rounded-md border border-white/10 bg-black/10 px-3 py-1 text-[var(--color-primary)] dark:bg-white/25">
+            <img
+              src="/language_logo/TypeScript.svg"
+              alt="TypeScript"
+              className="h-4 w-4"
+            />
+            TypeScript
           </span>
-          <span className="rounded-md border border-white/10 bg-white/10 px-3 py-1">
-            nginx
+          <span className="flex flex-row items-center gap-2 rounded-md border border-white/10 bg-black/10 px-3 py-1 text-[var(--color-primary)] dark:bg-white/25">
+            <img
+              src="/language_logo/JavaScript.svg"
+              alt="JavaScript"
+              className="h-4 w-4"
+            />
+            JavaScript
+          </span>
+          <span className="flex flex-row items-center gap-2 rounded-md border border-white/10 bg-black/10 px-3 py-1 text-[var(--color-primary)] dark:bg-white/25">
+            <img
+              src="/language_logo/Express.svg"
+              alt="Express"
+              className="h-4 w-4"
+            />
+            Express
+          </span>
+          <span className="flex flex-row items-center gap-2 rounded-md border border-white/10 bg-black/10 px-3 py-1 text-[var(--color-primary)] dark:bg-white/25">
+            <img src="/language_logo/SCSS.svg" alt="SCSS" className="h-4 w-4" />
+            SCSS
           </span>
         </div>
       </article>
